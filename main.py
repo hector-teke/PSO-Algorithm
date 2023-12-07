19accec14a32030aead5732bb3bbfaba14105a95
hector_teke <hectorguzman422@uma.es>
2023-12-07 10:53:31 +0100

    Add velocity to Individual parameters

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-"""- Hay que guardar la mejor solucion
-- Cada individuo tiene memoria de donde obtuvo la mejor solucion.
-Parametros: posicion, calidad, best_position, best_quality"""
+
 
 class Individual:
     def __init__(self):
@@ -8,9 +6,10 @@ class Individual:
         self.quality = None
         self.bposition = []
         self.bquality = None
+        self.velocity = 0
 
     def __str__(self):
-        return f"Individual:\nPosition={self.position}\nQuality={self.quality}\nBest Po.={self.bposition}\nBest Quality={self.bquality})"
+        return f"Individual:\nPosition={self.position}\nQuality={self.quality} \tVelocity:{self.velocity}\nBest Po.={self.bposition}\nBest Quality={self.bquality})"
 
 gbest = []
 gquality = None
