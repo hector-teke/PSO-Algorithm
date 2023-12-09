951293edabdfc77be036f6e044b453f20ffd157d
hector-teke <hectorguzman422@uma.es>
2023-12-09 20:10:21 +0100

    Algorithm is completed

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import math
-import random
 import numpy as np
 
 
 class ObjFunction:
 
-    def sphere(self, vector):
+    def sphere(self, vector, dim):
 
         sum = 0
 
@@ -14,9 +13,9 @@ class ObjFunction:
 
         return 1 / (1 + sum)    # Inverse. Cause we wanna found the minimum
 
-    def schwefel(self, vector):
+    def schwefel(self, vector, dim):
 
-        v1 = 4189.829   # Already multiplied for d=10
+        v1 = 418.9829 * dim   # Already multiplied for d=10
         sum = 0
 
         for e in vector:
@@ -27,9 +26,9 @@ class ObjFunction:
         return 1 / (1 + result)   # Inverse. Cause we wanna found the minimum
 
 
-    def rastrigin(self, vector):
+    def rastrigin(self, vector, dim):
 
-        v1 = 100    # 10 * d
+        v1 = 10 * dim    # 10 * d
         sum = 0
 
         for e in vector:
@@ -60,9 +59,9 @@ def create_first(bound, p_size, dimension, function):  # Returns the first popul
     for i in range(p_size):     #Number of individuals
         ind = Individual(dimension)
 
-        ind.position = np.random.uniform(-bound, bound, dimension)
+        ind.position = np.round(np.random.uniform(-bound, bound, dimension), decimals=4)
         ind.bposition = ind.position.copy()
-        ind.quality = function(ind.position)
+        ind.quality = function(ind.position, dimension)
         ind.bquality = ind.quality
 
         population.append(ind)
@@ -86,10 +85,19 @@ def weight(iter, itotal, wstart=0.9, wend=0.4):     #iter: current iteration   i
     return wstart - (((wstart - wend) * iter) / itotal)
 
 
-def new_position(ind, bound, function):       #Updates the position and quality of the individual
+def new_position(ind, bound, function, dimension):       #Updates the position and quality of the individual
     ind.position += ind.velocity
-    ind.position = np.round(np.clip(ind.position, -bound, bound), decimals=4)
-    ind.quality = function(ind.position)
+
+    # Clipping solution
+    #ind.position = np.round(np.clip(ind.position, -bound, bound), decimals=4)
+
+    # Random solution
+    for i in range(len(ind.position)):
+        if ind.position[i] < -bound or ind.position[i] > bound:
+            ind.position[i] = np.random.uniform(-bound, bound)
+    ind.position = np.round(ind.position, decimals=4)
+
+    ind.quality = function(ind.position, dimension)
 
 def update_velocity(ind, weight, bound, c1=1.49618, c2=1.49618):
     global gbest
@@ -102,7 +110,7 @@ def update_velocity(ind, weight, bound, c1=1.49618, c2=1.49618):
     ind.velocity = np.clip(new_velocity, -0.2*bound, 0.2*bound)
 
 
-def pso(bound=5.12, p_size=20, dimension=10, function=sum, iterations=1000):
+def pso(function, bound=5.12, p_size=20, dimension=10, iterations=1000):
     global gbest
     global gquality
     history = []
@@ -116,7 +124,7 @@ def pso(bound=5.12, p_size=20, dimension=10, function=sum, iterations=1000):
             #print("Current individual's position: ", ind.position, "Quality: ", ind.quality, "Vel: ", ind.velocity)
             w = weight(i, iterations)
             update_velocity(ind, w, bound)  #Update the velocity
-            new_position(ind, bound, function)        #Update the position and quality
+            new_position(ind, bound, function, dimension)        #Update the position and quality
 
             if ind.quality > ind.bquality:  #Update the individual's best
                 ind.bquality = ind.quality
@@ -125,6 +133,9 @@ def pso(bound=5.12, p_size=20, dimension=10, function=sum, iterations=1000):
         update_global(population)       #Update global best and global quality
         history.append(gquality)
         #print(f"{i}\t BestQuality = {gquality}")
+        if gquality == 1:
+            print("Optimal solution was reached at iteration: ", i)
+            return gbest, gquality, history
 
     #Return the best solution found
     return gbest, gquality, history
@@ -137,12 +148,14 @@ if __name__ == '__main__':
 
     f = ObjFunction()
 
-    best, quality, history = pso(bound=5.12, p_size=20, function=f.sphere, iterations=1000)
+    #best, quality, history = pso(dimension=10, bound=5.12, p_size=20, function=f.sphere, iterations=5000)
+    best, quality, history = pso(dimension=10, bound=500, p_size=1000, function=f.schwefel, iterations=15000)
 
     print("Best: ", best)
     print("Quality: ", quality)
-    print(history)
+    print("Lasts qualities: ", history[-5:])
 
+    #print(f.rastrigin([0,0,0.995,0,-0.995,0,0,0,0,0], 10))
 
 
 
